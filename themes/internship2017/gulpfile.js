4535dfa110431df06b5b620d5a047986fa016d92
parul sharma <parul.sharma@qed42.com>
2017-03-27 10:36:24 +0530

    responsive

--- themes/internship2017/gulpfile.js
+++ themes/internship2017/gulpfile.js
@@ -1,43 +1,53 @@
 var gulp = require('gulp');
-var scss = require('gulp-sass');
+var compass = require('gulp-compass');
+// var scss = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 var imagemin = require('gulp-imagemin');
-// var compass = require('browser-sync');
 
 
-gulp.task('scss', function(){
-  return gulp.src('scss/**/*.scss')
-    .pipe(scss())
-    .pipe(gulp.dest('css'))
-    .pipe(browserSync.reload({
-      stream: true
-    }))
-});
+gulp.task('compass', function() {
+    gulp.src('scss/**/*.scss')
+        .pipe(compass({
+            config_file: 'config.rb',
+            css: 'css',
+            sass: 'scss'
+        }))
+        .pipe(gulp.dest('css'))
+})
+
+// gulp.task('scss', function() {
+//     return gulp.src('scss/**/*.scss')
+//         .pipe(scss())
+//         .pipe(gulp.dest('css'))
+//         .pipe(browserSync.reload({
+//             stream: true
+//         }))
+// });
 
 
 gulp.task('browserSync', function() {
-  var files = [
-    'css/*',
-    'scss/*',
-    'index.php'
-  ]
+    var files = [
+        'css/*',
+        'scss/*',
+        'index.php'
+    ]
 
 
-  browserSync.init(files, {
-    proxy: "http://internship.2017/"
-  })
+    browserSync.init(files, {
+        proxy: "http://internship.2017/"
+    })
+});
+
+gulp.task('images', function() {
+    return gulp.src('images/**/*.+(png|jpg|gif|svg)')
+        .pipe(imagemin())
+        .pipe(gulp.dest('images'))
+});
+
+gulp.task('default', ['compass'], function() {
+    gulp.watch('scss/**/*.scss', ['compass']);
 });
 
 
-gulp.task('watch', ['browserSync','scss'], function (){
-  gulp.watch('scss/**/*.scss', ['scss']);
-  // Other watchers
-});
 
 
-
-gulp.task('images', function(){
-  return gulp.src('images/**/*.+(png|jpg|gif|svg)')
-  .pipe(imagemin())
-  .pipe(gulp.dest('images'))
-});
